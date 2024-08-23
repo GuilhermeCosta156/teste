8c3587864063f15799314080e9d5070a63acea34
Guilherme Costa <guicscosta@gmail.com>
2024-08-23 22:23:56 +0100

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Static Site</h1>
+      <p>This is a static website using Next.js with App Routing.</p>
+    </div>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '',
+    trailingSlash: true, // Optional: Ensures all routes end with a trailing slash
+  };
+  
+  export default nextConfig;
